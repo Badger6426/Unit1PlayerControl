92f1d5da57ba2628c88fe91b66d5866cf07e6615
Tyler Karthaeuser <tylerkart0407@gmail.com>
2023-04-11 13:42:21 -0400

    Camera follow

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Update is called once per frame 
+    public float speed = 20;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         //We'll move the vechile forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
     }
 }
